da9e18532754335a4ff61f798d58eb271728d97d
Kamran-Rafiq-Sofi <kamranrafiq805@gmail.com>
2023-02-02 16:16:50 +0530

    setting up view engine ejs

---
+++ controllers/likes_controller.js
@@ -0,0 +1,3 @@
+module.exports.loved=function(req,res){
+    res.end('<h1>Liked</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,10 @@ const homeController=require('../controllers/home_controller');
 console.log("router started");
 
 router.get('/', homeController.home);
+
+// from user.js
+router.use('/users',require('./users'));
+// from likes.js
+router.use('/likes',require('./likes'));
+
 module.exports=router;
